d9f12155af5aba14fff4117dae0f031823fb8e9f
MakerSquare Student <students@themakersquare.com>
2013-08-05 11:37:13 -0500

    Revert "Renamed user id to enthusiast id in Tattoo model, Added artist id."
    This reverts commit 92807b387042c247b5047fe8ac16c40ea2a890f6.

--- db/migrate/20130805150356_add_and_change_user_columns.rb
+++
@@ -1,6 +0,0 @@
-class AddAndChangeUserColumns < ActiveRecord::Migration
-  def change
-    rename_column :tattoos, :user_id, :enthusiast_id
-    add_column :tattoos, :artist_id, :integer
-  end
-end

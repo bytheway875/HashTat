2ca7a1cb5fb3e71d197c6976b1f72c28196240e6
bytheway875 <s.byrne2254@gmail.com>
2013-08-06 12:04:54 -0500

    Added more logic for portfolio model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,11 @@
 class Portfolio < ActiveRecord::Base
-  attr_accessible :first_name, :last_name, :
+  before_create :user_is_artist?
+  attr_accessible :first_name, :last_name, :city, :state, :tattoo_shop, :bio, :twitter, :instagram, :flickr, :website
+
+
+  private
+    def user_is_artist?
+      if current_user.is_artist == true
+    end
+  end
 end
